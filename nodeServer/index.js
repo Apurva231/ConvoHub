2a01180f88c633bbffdb662c0e6296a4e6410384
Apurva231 <apurvaamrutkar05@gmail.com>
2026-04-30 22:57:40 +0530

    fix: socket.io CDN, dynamic server URL, express static path

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,29 +1,31 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
 
-app.use(express.static('../')); // serves index.html, css/, js/, etc.
-
-http.listen(process.env.PORT || 8000);
+app.use(express.static(path.join(__dirname, '../')));
 
 const users = {};
 
-io.on('connection',socket=>{
-    socket.on('new-user-joined', name =>{
-        //console.log(name);
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
+        socket.broadcast.emit('user-joined', name);
     });
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('leave',users[socket.id]);
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave', users[socket.id]);
         delete users[socket.id];
     });
-})
+});
+
+http.listen(process.env.PORT || 8000, () => {
+    console.log('Server running on port', process.env.PORT || 8000);
+});
